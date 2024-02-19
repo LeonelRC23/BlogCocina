de381ca53dc96803c4590a5d35d35a4aff812eea
Leonel Cordero <leonelrcordero22@gmail.com>
2024-02-19 18:55:45 -0300

    Se creo la estructura base del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import BarraNav from "./common/BarraNav";
+
+function App() {
+  return (
+    <>
+      <div>
+        <BarraNav />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/common/BarraNav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BarraNav = () => {
+  return (
+    <div>
+      <div>
+        <img
+          src="https://drive.google.com/file/d/1tL6fdQDqn4qszzF7tJsxhr7g5HWfroRb/view?usp=sharing"
+          alt=""
+        />
+      </div>
+      <div>
+        <ul>
+          <li>Postres</li>
+          <li>Reposteria</li>
+          <li>Bebidas</li>
+          <li>Cocina</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default BarraNav;
